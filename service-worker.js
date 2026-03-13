920ca9f8cd321c6c3e4044b0b240878a85d2f3b0
hermansyahj68-jpg <hermansyahj68@gmail.com>
2026-03-13 10:01:48 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,27 @@
+const CACHE_NAME = "belanja-app-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./logo.png",
+  "./manifest.json",
+  "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"
+];
+
 self.addEventListener("install", function(event) {
-console.log("Service Worker Installed");
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+  );
 });
